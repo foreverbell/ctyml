a96dea9e7195e747d9df38dbe30a66757e7c99b6
foreverbell <dql.foreverbell@gmail.com>
2016-11-15 11:09:04 +0800

    grammar and visitor

--- src/common.h
+++ src/common.h
@@ -8,4 +8,19 @@ struct Location {
   Location(int number, int offset) : line_number(number), line_offset(offset) { }
 };
 
+template<class T>
+class Visitor;
 
+template<class T>
+class VisitableBase {
+ public:
+  virtual void accept(Visitor<T>* visitor) = 0;
+};
+
+template<class Base, class Derived>
+class Visitable : Base {
+ public:
+  void accept(Visitor<Base>* visitor) override {
+    visitor->visit(static_cast<Derived*>(this));
+  }
+};
